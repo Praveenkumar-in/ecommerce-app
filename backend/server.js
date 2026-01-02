697b79a09af22b3c595ceb1f0168ab82bd44c138
Praveenkumar-in <praveenkumar.xoffical@gmail.com>
2026-01-02 13:38:02 +0530

    git  commit -m

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ import orderRouter from "./routes/orderRoute.js"
 
 
 const app =express()
-const port = process.env.PORT||4000
+//const port = process.env.PORT||4000
 connectDB()
 connectCloudinary()
 
